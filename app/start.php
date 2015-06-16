ef476c9cbfbad21843d9e128d7e25c435f245ad1
darknawg63 <darknawg63@gmail.com>
2015-06-16 19:46:33 +0200

    PHP Authentication: Configuration (4/30)

--- app/start.php
+++ app/start.php
@@ -1,6 +1,7 @@
 <?php
 
 use Slim\Slim;
+use Noodlehaus\Config;
 
 session_cache_limiter(false);
 session_start();
@@ -11,5 +12,10 @@ define('INC_ROOT', dirname(__DIR__));
 
 require INC_ROOT . '/vendor/autoload.php';
 
-$app = new Slim();
+$app = new Slim([
+    'mode' => file_get_contents(INC_ROOT . '/mode.php')
+]);
 
+$app->configureMode($app->config('mode'), function() use ($app) {
+    $app->config = Config::load(INC_ROOT . "/app/config/{$app->mode}.php");
+});
